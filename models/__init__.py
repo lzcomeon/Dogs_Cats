87fe164d2c2264472ca8f45835f4855fa9dbc950
AchDream <lizcomeon@163.com>
2018-11-14 21:54:08 +0800

    completely copy book version

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .alexnet import AlexNet
+from .squeezenet import SqueezeNet
+from .resnet34 import ResNet34
---
+++ train.sh
@@ -0,0 +1,6 @@
+python3 main.py train \
+   --train-data-root=data/train/  \
+   --lr=0.005  \
+   --batch-size=32  \
+   --model='AlexNet'  \
+   --max-epoch = 20
